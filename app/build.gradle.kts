f4f4f8747b3fb611b05751940b4008372ef8cb7b
DT <dt52hhs@gmail.com>
2025-07-03 21:29:27 -0500

    Add CI for unit tests and coverage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,17 +79,21 @@ dependencies {
 tasks.register<JacocoReport>("jacocoTestReport") {
     dependsOn("testDebugUnitTest")
 
+    group = "verification"
+    description = "Generates JaCoCo test coverage report for the debug build."
+
     reports {
         xml.required.set(true)
         html.required.set(true)
     }
 
     val fileFilter = listOf("**/R.class", "**/R$*.class", "**/BuildConfig.*", "**/Manifest*.*")
-    val debugTree = fileTree("${buildDir}/intermediates/javac/debug/classes").apply {
+
+    val kotlinDebugTree = fileTree("${buildDir}/tmp/kotlin-classes/debug") {
         exclude(fileFilter)
     }
 
-    classDirectories.setFrom(files(debugTree))
-    sourceDirectories.setFrom(files("src/main/java"))
-    executionData.setFrom(files("${buildDir}/jacoco/testDebugUnitTest.exec"))
+    classDirectories.setFrom(files(kotlinDebugTree))
+    sourceDirectories.setFrom(files("src/main/java", "src/main/kotlin"))
+    executionData.setFrom(fileTree(buildDir).include("jacoco/testDebugUnitTest.exec"))
 }
